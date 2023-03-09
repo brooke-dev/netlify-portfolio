2cdb2c996d28f8ad1dd06763b53d33b77bb89f3f
Brooke Ross <brooke.ross2800@gmail.com>
2023-03-09 09:31:40 -0700

    connect with me form added

--- src/NavBarFolder/Connect.js
+++ src/NavBarFolder/Connect.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import './Connect.css'
 
 
 function Connect() {
   return (
   <section id="connect">
-    <h1> Connect Section Here</h1>
-    <p>CONNECT</p>
+    <h1> Connect With Me!</h1>
+    <form name="contact" method="POST" data-netlify="true">
+        <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <label>Your Role: <select name="role[]" multiple>
+            <option value="Recruiter">Recruiter</option>
+            <option value="Client">Client</option>
+            <option value="Hello">No role, just saying hi!</option>
+            </select></label>
+        </p>
+        <p>
+            <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
+   
   </section>
   );
 }

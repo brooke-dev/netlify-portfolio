53c32708d035205c52906cbf87a50c28bad7a2d7
Brooke Ross <brooke.ross2800@gmail.com>
2023-03-27 12:32:39 -0600

    required fields in contact form

--- src/NavBarFolder/Connect.js
+++ src/NavBarFolder/Connect.js
@@ -9,10 +9,10 @@ function Connect() {
     <form name="contact" method="post" data-netlify="true" >
         <input type="hidden" name="form-name" value="contact" />
         <p>
-            <label>Your Name: <input type="text" name="name" /></label>
+            <label>Your Name: <input type="text" name="name" required/></label>
         </p>
         <p>
-            <label>Your Email: <input type="email" name="email" /></label>
+            <label>Your Email: <input type="email" name="email" required /></label>
         </p>
         <p>
             <label>Your Role: <select name="role[]" multiple>
@@ -22,7 +22,7 @@ function Connect() {
             </select></label>
         </p>
         <p>
-            <label>Message: <textarea name="message"></textarea></label>
+            <label>Message: <textarea name="message" required></textarea></label>
         </p>
         <p>
             <button type="submit">Send</button>

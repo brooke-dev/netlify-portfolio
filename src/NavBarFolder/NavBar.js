06f39ab7d99ffc7d6af3bd0c97cf4531fcf4ceed
Brooke Ross <brooke.ross2800@gmail.com>
2023-04-11 14:52:46 -0600

    navbar with useState React Hook

--- src/NavBarFolder/NavBar.js
+++ src/NavBarFolder/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NavBar.css'; // Import CSS styles for the component
 // import { NavLink } from 'react-router-dom';
 // import darkmode from './iconImages/darkmode.png'
@@ -8,21 +8,27 @@ import { Link } from 'react-scroll';
 
 
 function NavBar({isDarkMode, setIsDarkMode}) {
-  const hamburger = document.querySelector(".hamburger");
-  const bar1 = document.querySelector(".bar1");
-  const bar2 = document.querySelector(".bar2");
-  const bar3 = document.querySelector(".bar3");
-  const mobileNav = document.querySelector(".mobileNav");
+  // const hamburger = document.querySelector(".hamburger");
+  // const bar1 = document.querySelector(".bar1");
+  // const bar2 = document.querySelector(".bar2");
+  // const bar3 = document.querySelector(".bar3");
+  // const mobileNav = document.querySelector(".mobileNav");
 
-  if (hamburger) {
-    hamburger.addEventListener("click", () => {
-      bar1.classList.toggle("animateBar1");
-      bar2.classList.toggle("animateBar2");
-      bar3.classList.toggle("animateBar3");
-      mobileNav.classList.toggle("openDrawer");
-    });
-    console.log("clicked")
-  }
+  // if (hamburger) {
+  //   hamburger.addEventListener("click", () => {
+  //     bar1.classList.toggle("animateBar1");
+  //     bar2.classList.toggle("animateBar2");
+  //     bar3.classList.toggle("animateBar3");
+  //     mobileNav.classList.toggle("openDrawer");
+  //   });
+  //   console.log("clicked")
+  // }
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setIsOpen(!isOpen);
+  };
 
 
   return (
@@ -36,13 +42,13 @@ function NavBar({isDarkMode, setIsDarkMode}) {
               <Link to="projects" smooth={true} duration={500}>Projects</Link>
               <Link to="connect" smooth={true} duration={500}>Connect with Me!</Link>
             </nav>
-            <div className="hamburger">
-                <div className="bar1"></div>
-                <div className="bar2"></div>
-                <div className="bar3"></div>
+            <div className="hamburger" onClick={toggleMobileNav}>
+              <div className={`bar1 ${isOpen ? 'animateBar1' : ''}`}></div>
+              <div className={`bar2 ${isOpen ? 'animateBar2' : ''}`}></div>
+              <div className={`bar3 ${isOpen ? 'animateBar3' : ''}`}></div>
             </div>
         </header>
-        <nav className="mobileNav">
+        <nav className={`mobileNav ${isOpen ? 'openDrawer' : ''}`}>
           <Link to="home" smooth={true} duration={500}>Home</Link>
           <Link to="about" smooth={true} duration={500}>About</Link>
           <Link to="skills" smooth={true} duration={500}>Skills</Link>
